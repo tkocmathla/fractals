318517413f676498f929c9d919b7d264eceb77ed
tkocmathla <matt.t.grimm@gmail.com>
2017-09-16 09:30:46 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject fractals "0.1.0-SNAPSHOT"
+  :description "Fractal explorations with Quil"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "2.6.0"]])
---
+++ src/fractals/core.clj
@@ -0,0 +1,4 @@
+(ns fractals.core
+  (:require
+    [quil.core :as q]
+    [quil.middleware :as qm]))
